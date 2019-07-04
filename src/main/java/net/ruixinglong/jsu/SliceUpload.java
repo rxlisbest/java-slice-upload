85a343f184ce04d706fe518bdcfdadd34fd0ebae
Roy <rxlisbest@gmail.com>
2019-07-04 14:25:53 +0800

    HttpServletRequest

--- src/main/java/net/ruixinglong/jsu/SliceUpload.java
+++ src/main/java/net/ruixinglong/jsu/SliceUpload.java
@@ -1,27 +1,31 @@
 package net.ruixinglong.jsu;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 
 public class SliceUpload {
 
     protected Storage upload;
 
-    @Autowired(required=false)
-    private HttpServletRequest request;
-
     SliceUpload(String directory) {
         this.upload = new Storage(directory);
     }
 
-    public String save() throws IOException {
-        System.out.println(request.getContentType());
-        InputStream f = new FileInputStream("/Users/apple/Pictures/test/php.jpg");
-        String tempDir = System.getProperty("java.io.tmpdir");
-        return this.upload.setKey("test.jpg").setTempDir(tempDir).setName("test.jpg").setStream(f).save();
+    public String save() {
+        try {
+            HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder
+                    .getRequestAttributes()).getRequest();
+            System.out.println(request);
+            InputStream f = new FileInputStream("/Users/apple/Pictures/test/php.jpg");
+            String tempDir = System.getProperty("java.io.tmpdir");
+            return this.upload.setKey("test.jpg").setTempDir(tempDir).setName("test.jpg").setStream(f).save();
+        } catch (Exception e) {
+            return "";
+        }
     }
 }
--- src/test/java/net/ruixinglong/jsu/SliceUploadController.java
+++ src/test/java/net/ruixinglong/jsu/SliceUploadController.java
@@ -3,10 +3,16 @@ package net.ruixinglong.jsu;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
 @RestController
 public class SliceUploadController {
     @RequestMapping("/slice-upload")
-    public String index() {
+    public String index(HttpServletRequest request) {
+        System.out.println(request.getRequestURI());
+        SliceUpload sliceUpload = new SliceUpload("/Users/apple/Pictures/test/");
+        sliceUpload.save();
         return "Hello World";
     }
 }
